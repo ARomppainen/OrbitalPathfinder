03e3edee9a033ae282076901f02a6f74df5b1d43
Aleksi Romppainen <aromppa@gmail.com>
2016-05-15 12:29:24 +0300

    commit

--- src/com/romppainen/aleksi/orbitalpathfinder/Main.java
+++ src/com/romppainen/aleksi/orbitalpathfinder/Main.java
@@ -6,16 +6,14 @@ package com.romppainen.aleksi.orbitalpathfinder;
  */
 public class Main {
     public static void main(String[] args) {
-        OrbitalPathfinder op = new OrbitalPathfinder();
-        op.calcPath("data.txt");
+        //OrbitalPathfinder op = new OrbitalPathfinder();
+        //op.calcPath("data.txt");
         
-        /*
         if (args.length > 0) {
             OrbitalPathfinder op = new OrbitalPathfinder();
             op.calcPath(args[0]);
         } else {
             System.out.println("Usage: java -jar OrbitalPathfinder filename");
         }
-        */
     }
 }
